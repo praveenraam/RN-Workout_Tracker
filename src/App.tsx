8c9697ebb3ae11314ef3b158816ab4c2a19b19cd
praveenraam <ckpraveenraam@gmail.com>
2024-09-23 11:03:29 +0530

    Navigation is added to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
-import { styled } from 'nativewind';
 import Home from './screens/Home';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const StyledView = styled(View);
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <StyledView>
-      <Home />
-    </StyledView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
