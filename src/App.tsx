1c0551dbd4a1f7bff0a3444c638863141b838042
praveenraam <ckpraveenraam@gmail.com>
2024-10-07 11:54:24 +0530

    #14, Icon and name added at the top of the Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Home from './screens/Home';
-
+import { Image, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
 import MuscleList from './screens/MusclesList';
 import PerMuscleWorkedList from './screens/PerMuscleWorkList';
 
@@ -14,7 +14,16 @@ const App = () => {
     <GestureHandlerRootView className="flex-1">
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerLeft: () => (<Text className="text-xl font-bold text-black ml-2">Workout App</Text>),
+              headerRight: () => (<Image source={require('../assets/calendar.png')} className="w-7 h-7 mr-2" />),
+              headerTitle: '', // This removes the default title
+              headerTitleAlign: 'center',
+            }}
+          />
           <Stack.Screen name="Muscles List" component={MuscleList} />
           <Stack.Screen name="Muscle Workout List" component={PerMuscleWorkedList} />
         </Stack.Navigator>
