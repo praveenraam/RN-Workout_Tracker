ca6d50dc176b8fbb904cb24e1eee22f091029d7e
praveenraam <ckpraveenraam@gmail.com>
2024-10-07 15:27:31 +0530

    #14, Code for Navigating to Calendar comp is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, Pressable } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import MuscleList from './screens/MusclesList';
 import PerMuscleWorkedList from './screens/PerMuscleWorkList';
+import Calendar from './screens/Calendar';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,15 +18,22 @@ const App = () => {
           <Stack.Screen
             name="Home"
             component={Home}
-            options={{
+            options={({ navigation }) => ({
               headerLeft: () => (<Text className="text-xl font-bold text-black ml-2">Workout App</Text>),
-              headerRight: () => (<Image source={require('../assets/calendar.png')} className="w-7 h-7 mr-2" />),
+              headerRight: () => ( 
+              <Pressable onPress={() => navigation.navigate('Calendar')}>
+                <Image 
+                  source={require('../assets/calendar.png')} 
+                  className="w-7 h-7 mr-2" 
+                />
+              </Pressable>),
               headerTitle: '', // This removes the default title
               headerTitleAlign: 'center',
-            }}
+            })}
           />
           <Stack.Screen name="Muscles List" component={MuscleList} />
           <Stack.Screen name="Muscle Workout List" component={PerMuscleWorkedList} />
+          <Stack.Screen name="Calendar" component={Calendar} />
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
---
+++ src/screens/Calendar.tsx
@@ -0,0 +1,13 @@
+import {  Text, View } from 'react-native';
+import React from 'react';
+
+const Calendar = () => {
+  return (
+    <View>
+      <Text>Calendar</Text>
+    </View>
+  );
+};
+
+export default Calendar;
+
