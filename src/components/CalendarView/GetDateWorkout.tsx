e3275a100fe50b1ee8db9df4ac0163db5428d2e7
praveenraam <ckpraveenraam@gmail.com>
2024-10-08 10:43:33 +0530

    #15, Code for the getting workout is done

--- src/components/CalendarView/GetDateWorkout.tsx
+++ src/components/CalendarView/GetDateWorkout.tsx
@@ -1,11 +1,22 @@
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/native';
+import { getData } from '../../utils/asyncStorageUtils';
 
 const GetDateWorkout = () => {
 
   const route = useRoute();
   const {selectedDate} = route.params;
+  const [workoutsList,setWorkoutList] = useState('');
+
+  useEffect(() => {
+    const loadData = async () => {
+        const jsonValue  = await getData(selectedDate);
+        setWorkoutList(jsonValue);
+        // console.log(workoutsList);
+    };
+    loadData();
+  },[selectedDate]);
 
   return (
     <View>
