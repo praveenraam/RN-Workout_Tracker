2ac7bd9a21122b946fc16c76556dd11ef1b33546
praveenraam <ckpraveenraam@gmail.com>
2024-10-08 09:50:45 +0530

    #14, That component styling is completed

--- src/screens/CalendarScreen.tsx
+++ src/screens/CalendarScreen.tsx
@@ -15,14 +15,23 @@ const CalendarScreen = () => {
   };
 
   return (
-    <View>
+    <View className="flex-1 bg-black h-screen">
       <StyledCalender
-
         onDayPress={handleDayPress}
-        // theme={
-
-        // }
-
+        theme={{
+          backgroundColor: '#000000',         // Background of the calendar
+          calendarBackground: '#000000',      // Background of the calendar cells
+          textSectionTitleColor: '#ffffff',   // Month & week day title color
+          selectedDayBackgroundColor: '#00adf5',  // Background color for selected day
+          selectedDayTextColor: '#ffffff',    // Text color for the selected day
+          todayTextColor: '#00adf5',          // Text color for today's date
+          dayTextColor: '#ffffff',            // Default day text color
+          textDisabledColor: '#d9e1e8',       // Color for disabled dates
+          dotColor: '#00adf5',                // Dot indicator color
+          selectedDotColor: '#ffffff',        // Selected date dot color
+          arrowColor: 'white',                // Arrow color for navigating months
+          monthTextColor: 'white',
+        }}
       />
     </View>
   );
