2b4c0caad108db3e8f6209083ce4047d9d86581f
praveenraam <ckpraveenraam@gmail.com>
2024-10-09 11:33:04 +0530

    #15, Code for viewing the muscle group worked is added

--- src/components/CalendarView/GetDateWorkout.tsx
+++ src/components/CalendarView/GetDateWorkout.tsx
@@ -6,11 +6,15 @@ import { styled } from 'nativewind';
 import { formatDate } from '../home/PreviousWorkouts';
 import { FlatList } from 'react-native-gesture-handler';
 
+
+const muscleData = require('../../workoutData/muscleNames.json');
+
 const GetDateWorkout = () => {
 
   const route = useRoute();
   const {selectedDate} = route.params;
   const [workoutsList, setWorkoutList] = useState<string[]>([]);
+  const [musclesWorked, setMuscleWorked] = useState<string[]>([]);
 
   const StyledView = styled(View);
   const StyledText = styled(Text);
@@ -19,11 +23,19 @@ const GetDateWorkout = () => {
     const loadData = async () => {
         const jsonValue  = await getData(selectedDate);
 
-        if(jsonValue){
+        if (jsonValue) {
           setWorkoutList(jsonValue.workouts);
+
+          const uniqueMuscles = Array.from(
+            new Set(
+              jsonValue.workouts.map((workout: string) => muscleData[workout] || 'Unknown')
+            )
+          );
+          setMuscleWorked(uniqueMuscles);
         }
         else{
           setWorkoutList([]);
+          setMuscleWorked([]);
         }
     };
     loadData();
@@ -42,11 +54,17 @@ const GetDateWorkout = () => {
       </StyledText>
 
       {workoutsList.length > 0 ? (
-          <FlatList
-            data={workoutsList}
-            renderItem={renderWorkoutItems}
-            keyExtractor={(item,index) => index.toString()}
-          />
+          <>
+            <StyledText className="font-bold text-2xl text-white">
+              Muscles Worked:
+            </StyledText>
+            <StyledText className="text-white text-2xl mb-4">{musclesWorked.join(', ')}</StyledText>
+            <FlatList
+              data={workoutsList}
+              renderItem={renderWorkoutItems}
+              keyExtractor={(item,index) => index.toString()}
+            />
+          </>
         ) : (
           <StyledText className="text-lg text-white">No workouts available</StyledText>
         )
