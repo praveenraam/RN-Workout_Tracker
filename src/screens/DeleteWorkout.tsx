87d5c83e044847e0ff79ef9d2db521bd602833cd
praveenraam <ckpraveenraam@gmail.com>
2024-10-17 11:41:34 +0530

    #22 #20, Code for fetching the data from asyncStorage is done

--- src/screens/DeleteWorkout.tsx
+++ src/screens/DeleteWorkout.tsx
@@ -1,8 +1,32 @@
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const DeleteWorkout = () => {
-  return (
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const muscleGroups = ['Back','Biceps','Legs','Shoulders','Triceps','Chest'];
+            let workoutsData: any[] = [];
+
+            for(const muscle of muscleGroups){
+                try{
+                    const data = await AsyncStorage.getItem(muscle);
+                    if(data){
+                        const parseData = JSON.parse(data);
+                        workoutsData = [...workoutsData, ...parseData];
+                    }
+                }
+                catch(error){
+                    console.log(error);
+                }
+            }
+            console.log(workoutsData);
+        };
+        fetchData();
+    });
+
+    return (
     <View>
       <Text>DeleteWorkout</Text>
     </View>
