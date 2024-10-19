6874994056f5c04cbc66f2e153a17e8c52ae4a2c
praveenraam <ckpraveenraam@gmail.com>
2024-10-19 14:08:40 +0530

    #22, Code for deleting the custom added workout is done

--- src/screens/DeleteWorkout.tsx
+++ src/screens/DeleteWorkout.tsx
@@ -1,7 +1,9 @@
-import { Text, View, Image, ScrollView, FlatList, Pressable  } from 'react-native';
+import { Text, View, Image, ScrollView, FlatList, Pressable, Alert  } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { styled } from 'nativewind';
+import { useNavigation } from '@react-navigation/native';
+
 
 const StyledImage = styled(Image);
 const StyledView = styled(View);
@@ -11,7 +13,40 @@ const StyledFlatList = styled(FlatList);
 
 const DeleteWorkout = () => {
 
+    const navigation = useNavigation();
+
     const [workoutsData,setWorkoutsData] = useState<any[]>([]);
+    // const [ChangedWorkout,setChangedWorkout] = useState<any[]>([]);
+
+    const handlePress = async (MuscleWorked: string, WorkoutName: string) => {
+        let ChangedWorkout: any[] = []; // Initialize as an empty array
+        try {
+            const Data = await AsyncStorage.getItem(MuscleWorked);
+            const parsedData = Data ? JSON.parse(Data) : null;
+
+            await AsyncStorage.removeItem(MuscleWorked);
+
+            if (parsedData && Array.isArray(parsedData)) {
+                parsedData.forEach((item) => {
+                    if (item.name !== WorkoutName) {
+                        ChangedWorkout = [...ChangedWorkout, item]; // Use the correct variable name
+                    }
+                });
+                await AsyncStorage.setItem(MuscleWorked, JSON.stringify(ChangedWorkout)); // Stringify the array
+                Alert.alert('Success', 'Workout has been successfully deleted.', [
+                    {
+                        text: 'OK',
+                        onPress: () => navigation.goBack(), // Navigate back after alert
+                    },
+                ]);
+            }
+            else {
+                console.log('No data found');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -48,7 +83,7 @@ const DeleteWorkout = () => {
                             <StyledText className="text-sm text-white mb-1">Muscle : {item.muscleWorked}</StyledText>
                             <StyledText className="text-sm text-white mb-3">Equipment : {item.equipment}</StyledText>
                         </StyledView>
-                        <Pressable>
+                        <Pressable onPress={() => handlePress(item.muscleWorked,item.name)}>
                             <StyledImage className="w-8 h-8 right-3" source={require('../../assets/x-mark.png')} />
                         </Pressable>
                     </StyledView>
